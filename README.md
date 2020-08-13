a54bcdf84b584689a9b342142b444d39f0396621
MuslimMujahid <muslimmujahid1712@gmail.com>
2020-08-13 19:15:55 +0700

    workig on downside flip connected pawns

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Othello-Game
+
+# Development
+- Board created
+- Game info created (still hard coded)
+- create pawn color respective to player at clicked box\
+- flip connected pawns upside
+
+# Working on
+- flip connected pawns downside.
+    problem: box element return null
--- app.js
+++ app.js
@@ -1,9 +1,76 @@
-// const board = document.querySelector('.board')
-// for (let row = 0; row < 8; row++) {
-//     for (let col = 0; col < 8; col++) {
-//         let newEl = document.createElement('div')
-//         newEl.classList.add('box')
-//         newEl.classList.add(`box-${row}-${col}`)
-//         board.appendChild(newEl)
-//     }
-// }
+class Othello {
+    constructor(playerOneName, playerTwoName) {
+        this.playerNames = [playerOneName, playerTwoName]
+    }
+
+    init() {
+        this.round = 0
+        this.playerScores = [0, 0]
+        this.turn = 0
+        this.lastCreatedPawn = undefined
+        this.playerColors = ['black', 'white']
+    }
+
+    changeTurn() {
+        this.turn = Math.abs(this.turn - 1)
+    }
+
+    createPawn()  {
+        let pawn = document.createElement('div')
+        pawn.classList.add('pawn')
+        pawn.classList.add(this.playerColors[this.turn])
+        this.lastCreatedPawn = pawn
+        return pawn
+    } 
+
+    flipConnectedPawns() {
+        const parent = this.lastCreatedPawn.parentElement
+        const [pawnY, pawnX] = parent.classList[1].split('box-')[1].split('-')
+        const playerColor = this.playerColors[this.turn]
+        const enemyColor = this.playerColors[Math.abs(this.turn-1)]
+
+        // check upside
+        for (let y = parseInt(pawnY)-1; y >= 0; y--) {
+            let pawn = document.querySelector(`.box-${y}-${pawnX}`).firstChild
+            if (pawn == null) break
+            if (pawn.classList.contains(playerColor)) {
+                for (let yR = pawnY-1; yR > y; yR--) {
+                    let pawnFlip = document.querySelector(`.box-${yR}-${pawnX}`).firstChild
+                    pawnFlip.classList.remove(enemyColor)
+                    pawnFlip.classList.add(playerColor)
+                }
+            }
+        }
+
+        // check downside
+        for (let y = parseInt(pawnY) + 1; y < 8; y++) {
+            // console.log(y)
+            console.log(`.box-${y}-${pawnX}`)
+            let pawn = document.querySelector(`.box-${y}-${pawnX}`).firstChild
+            if (pawn == null) break
+            if (pawn.classList.contains(playerColor)) {
+                for (let yR = pawnY+1; yR < y; yR++) {
+                    let pawnFlip = document.querySelector(`.box-${yR}-${pawnX}`).firstChild
+                    pawnFlip.classList.remove(enemyColor)
+                    pawnFlip.classList.add(playerColor)
+                }
+            }
+        }
+    }
+}
+
+const game = new Othello('Muslim', 'Dayat')
+game.init()
+
+const boxes = document.querySelectorAll('.box')
+
+boxes.forEach(box => {
+    box.addEventListener('click', () => {
+
+        if (box.childElementCount == 0) {
+            box.appendChild(game.createPawn())
+            game.flipConnectedPawns()
+            game.changeTurn()
+        }
+    })
+})
