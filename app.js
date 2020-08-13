34f5af1dc99090c6ca6b851a22fd1ff4d98d51c0
MuslimMujahid <muslimmujahid1712@gmail.com>
2020-08-13 17:18:01 +0700

    created board and game info ui

---
+++ app.js
@@ -0,0 +1,9 @@
+// const board = document.querySelector('.board')
+// for (let row = 0; row < 8; row++) {
+//     for (let col = 0; col < 8; col++) {
+//         let newEl = document.createElement('div')
+//         newEl.classList.add('box')
+//         newEl.classList.add(`box-${row}-${col}`)
+//         board.appendChild(newEl)
+//     }
+// }
