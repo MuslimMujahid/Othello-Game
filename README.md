94e81d31236a0485d181e01a8f9b8431df7a4038
MuslimMujahid <muslimmujahid1712@gmail.com>
2020-08-14 08:42:54 +0700

    update

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 
 # Development
 - Board created
-- Game info created (still hard coded)
-- create pawn color respective to player at clicked box\
-- flip connected horizontal and vertical
+- Game info created
+- create pawn color respective to player at clicked box
+- flip connected pawns
 
 # Working on
-- flip connected top-left side.
-    problem: infinite loop
--- app.js
+++ app.js
@@ -1,24 +1,68 @@
-class Othello {
-    constructor(playerOneName, playerTwoName) {
-        this.playerNames = [playerOneName, playerTwoName]
+class Player {
+    constructor(name, score, color) {
+        this.name = name
+        this.score = score
+        this.color = color
     }
+
+    incrementScore() {
+        this.score++
+    }
+
+    decrementScore() {
+        this.score--
+    }
+}
+
+class UserInterface {
+    constructor(interfaces) {
+        if (interfaces != null) {
+            this.map = interfaces
+        } else {
+            this.map = {}
+        }
+    }
+
+    addInterface(name, element) {
+        this.map[name] = element
+    }
+
+    addInterfaces(elements) {
+        this.map = {...this.map, ...elements}
+    }
+
+    getInterface(elm) {
+        return this.map[elm]
+    }
+
+    setInterface(interfaceName, value) {
+        this.map[interfaceName].innerText = value
+    }
+}
+
+class Othello {
+    constructor(playerOneName, playerTwoName, UserInterface) {
+            
+            this.playerOne = new Player(playerOneName, 0, 'black')
+            this.playerTwo = new Player(playerTwoName, 0, 'white')
+            this.UserInterface = UserInterface
+        }
 
     init() {
         this.round = 0
-        this.playerScores = [0, 0]
-        this.turn = 0
+        this.curPlayer = this.playerOne
+        this.nextPlayer = this.playerTwo
         this.lastCreatedPawn = undefined
-        this.playerColors = ['black', 'white']
     }
 
     changeTurn() {
-        this.turn = Math.abs(this.turn - 1)
+        [this.curPlayer, this.nextPlayer] = [this.nextPlayer, this.curPlayer] 
     }
 
     createPawn()  {
         let pawn = document.createElement('div')
-        pawn.classList.add('pawn')
-        pawn.classList.add(this.playerColors[this.turn])
+        pawn.classList.add('pawn', this.curPlayer.color)
+        this.curPlayer.incrementScore();
         this.lastCreatedPawn = pawn
         return pawn
     } 
@@ -26,78 +70,78 @@ class Othello {
     flipConnectedPawns() {
         const parent = this.lastCreatedPawn.parentElement
         const [pawnY, pawnX] = parent.classList[1].split('box-')[1].split('-')
-        const playerColor = this.playerColors[this.turn]
-        const enemyColor = this.playerColors[Math.abs(this.turn-1)]
 
-        // check upside
-        for (let y = parseInt(pawnY)-1; y >= 0; y--) {
-            let pawn = document.querySelector(`.box-${y}-${pawnX}`).firstChild
-            if (pawn == null) break
-            if (pawn.classList.contains(playerColor)) {
-                for (let yR = pawnY-1; yR > y; yR--) {
-                    let pawnFlip = document.querySelector(`.box-${yR}-${pawnX}`).firstChild
-                    pawnFlip.classList.remove(enemyColor)
-                    pawnFlip.classList.add(playerColor)
+        const flipXY = (xI, yI) => {
+            const xMax = xI < 0 ? -1 : 8
+            const yMax = yI < 0 ? -1 : 8
+
+            let x = parseInt(pawnX) + xI
+            let y = parseInt(pawnY) + yI
+            while (x != xMax || y != yMax) {
+                try {
+                    let pawn = document.querySelector(`.box-${y}-${x}`).firstChild
+                    if (pawn == null) break
+                    if (pawn.classList.contains(this.curPlayer.color)) {
+                        let xR = parseInt(pawnX) + xI
+                        let yR = parseInt(pawnY) + yI
+                        while (xR != x || yR != y) {
+                            // console.log(xR, yR)
+                            let pawnFlip = document.querySelector(`.box-${yR}-${xR}`).firstChild
+                            pawnFlip.classList.remove(this.nextPlayer.color)
+                            pawnFlip.classList.add(this.curPlayer.color)
+                            this.curPlayer.incrementScore()
+                            this.nextPlayer.decrementScore()
+                        
+                            xR += xI
+                            yR += yI
+                        }
+                    }
+                } catch(e) {
+                    break
                 }
+
+                x += xI
+                y += yI
             }
         }
 
-        // check downside
-        for (let y = parseInt(pawnY) + 1; y < 8; y++) {
-            let pawn = document.querySelector(`.box-${y}-${pawnX}`).firstChild
-            if (pawn == null) break
-            if (pawn.classList.contains(playerColor)) {
-                for (let yR = parseInt(pawnY) + 1; yR < y; yR++) {
-                    let pawnFlip = document.querySelector(`.box-${yR}-${pawnX}`).firstChild
-                    pawnFlip.classList.remove(enemyColor)
-                    pawnFlip.classList.add(playerColor)
-                }
-            }
-        }
+        flipXY(0, -1) // up
+        flipXY(0, 1) // down
+        flipXY(-1, 0) // left
+        flipXY(1, 0) // right
+        flipXY(1, 1) // bottom-right
+        flipXY(1, -1) // top-right
+        flipXY(-1, 1) // bottom-left
+        flipXY(-1, -1) // top-left
+    }
 
-        // check righside
-        for (let x = parseInt(pawnX) + 1; x < 8; x++) {
-            let pawn = document.querySelector(`.box-${pawnY}-${x}`).firstChild
-            if (pawn == null) break
-            if (pawn.classList.contains(playerColor)) {
-                for (let xR = parseInt(pawnX) + 1; xR < x; xR++) {
-                    let pawnFlip = document.querySelector(`.box-${pawnY}-${xR}`).firstChild
-                    pawnFlip.classList.remove(enemyColor)
-                    pawnFlip.classList.add(playerColor)
-                }
-            }
-        }
-
-        // check leftside
-        for (let x = parseInt(pawnX)-1; x >= 0; x--) {
-            let pawn = document.querySelector(`.box-${pawnY}-${x}`).firstChild
-            if (pawn == null) break
-            if (pawn.classList.contains(playerColor)) {
-                for (let xR = pawnX-1; xR > x; xR--) {
-                    let pawnFlip = document.querySelector(`.box-${pawnY}-${xR}`).firstChild
-                    pawnFlip.classList.remove(enemyColor)
-                    pawnFlip.classList.add(playerColor)
-                }
-            }
-        }
-
-        // check top-left side
-        for (let x = parseInt(pawnX)-1, y = parseInt(pawnY)-1; x >= 0 && y >= 0; x--, y--) {
-            let pawn = document.querySelector(`.box-${y}-${x}`).firstChild
-            if (pawn == null) break
-            if (pawn.classList.contains(playerColor)) {
-                for (let xR = parseInt(pawnX)-1, yR = parseInt(pawnY)-1; xR > x && yR > y; x--, y--) {
-                    let pawnFlip = document.querySelector(`.box-${yR}-${xR}`).firstChild
-                    pawnFlip.classList.remove(enemyColor)
-                    pawnFlip.classList.add(playerColor)
-                }
-            }
-        }
+    updateDisplay() {
+        this.UserInterface.setInterface('playerOneName', this.playerOne.name)
+        this.UserInterface.setInterface('playerTwoName', this.playerTwo.name)
+        this.UserInterface.setInterface('playerOneScore', this.playerOne.score)
+        this.UserInterface.setInterface('playerTwoScore', this.playerTwo.score)
+        this.UserInterface.setInterface('round', this.round)
     }
 }
 
-const game = new Othello('Muslim', 'Dayat')
+const board = document.querySelector('.board')
+const playerOneNameElement = document.querySelector('.playerOne .playerName')
+const playerOneScoreElement = document.querySelector('.playerOne .playerScore')
+const playerTwoNameElement = document.querySelector('.playerTwo .playerName')
+const playerTwoScoreElement = document.querySelector('.playerTwo .playerScore')
+const roundElement = document.querySelector('.roundInfo')
+
+const userInterface = new UserInterface({
+    'board': board,
+    'playerOneName': playerOneNameElement,
+    'playerTwoName': playerTwoNameElement,
+    'playerOneScore': playerOneScoreElement,
+    'playerTwoScore': playerTwoScoreElement,
+    'round': roundElement
+})
+const game = new Othello('Muslim', 'Dayat', userInterface)
 game.init()
+game.updateDisplay()
 
 const boxes = document.querySelectorAll('.box')
 
@@ -108,6 +152,7 @@ boxes.forEach(box => {
             box.appendChild(game.createPawn())
             game.flipConnectedPawns()
             game.changeTurn()
+            game.updateDisplay()
         }
     })
 })
